e6f25d406f4bf059994c247013f37874917451c9
Vladimir Louis <vovangus1235@gmail.com>
2021-08-30 23:37:36 +0300

    build: add method to render pixels when you resize the window

--- code/build.bat
+++ code/build.bat
@@ -4,7 +4,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary
 mkdir ..\build
 pushd ..\build
 
-cl -Zi ..\code\win32_byhand.cpp User32.lib gdi32.lib
+cl -FC -Zi ..\code\win32_byhand.cpp User32.lib gdi32.lib
 
 popd
 
--- code/win32_byhand.cpp
+++ code/win32_byhand.cpp
@@ -1,64 +1,117 @@
 #include <Windows.h>
+#include <stdint.h>
 
 #define internal static
 #define local_persist static
 #define global_variable static
 
-//TODO: global for the time
+// NOTE: WTF? WHAT IS THE SEQUENCE OF WORDS BELOW:
+typedef unsigned char uint8;
+
+// TODO: global for the time
 global_variable bool running;
 
 global_variable BITMAPINFO bitmapInfo;
 global_variable void *bitmapMemory;
-global_variable HBITMAP bitmapHandle;
-global_variable HDC bitmapDeviceContext;
+
+// NOTE don't forget to move it from global variables
+global_variable int bitmapWidth;
+global_variable int bitmapHeight;
 
 
 internal void win32resizeDIBSelection(int width, int height)
 {
 
-    //don't free first, free after, then free if that fails
-    //free our DIBSection
-    
-    if (bitmapHandle)
+    // TODO don't free first, free after, then free if that fails
+    // free our DIBSection
+
+
+    if (bitmapMemory)
     {
-        DeleteObject(bitmapHandle);
-    }
-    else
-    {
-        bitmapDeviceContext = CreateCompatibleDC(0);
+        // NOTE think about page protection to prevent making bugs
+        
+        VirtualFree(bitmapMemory, 0, MEM_RELEASE); 
     }
 
+    bitmapWidth = width;
+    bitmapHeight = height;
+
     bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo.bmiHeader);
-    bitmapInfo.bmiHeader.biWidth = width;
-    bitmapInfo.bmiHeader.biHeight = height;
+    bitmapInfo.bmiHeader.biWidth = bitmapWidth;
+    // NOTE with negative `bitmapHeight` it goes top-down DIB
+    bitmapInfo.bmiHeader.biHeight = -bitmapHeight;
     bitmapInfo.bmiHeader.biPlanes = 1;
     bitmapInfo.bmiHeader.biBitCount = 32;
     bitmapInfo.bmiHeader.biCompression = BI_RGB;
-
-    // if it's already a global variable the parameters below are set to zeros automatically
-
-    // bitmapInfo.bmiHeader.biSizeImage = 0;
-    // bitmapInfo.bmiHeader.biXPelsPerMeter = 0;
-    // bitmapInfo.bmiHeader.biYPelsPerMeter = 0;
-    // bitmapInfo.bmiHeader.biClrUsed = 0;
-    // bitmapInfo.bmiHeader.biClrImportant = 0;
-
     
+    // NOTE we need memory, but the question is "how much memory do we need?"
 
+    int bytesPerPixel = 4;
+    int bitmapMemorySize = (bitmapWidth * bitmapHeight) * bytesPerPixel;
 
-    bitmapHandle = CreateDIBSection(bitmapDeviceContext, &bitmapInfo, DIB_RGB_COLORS, &bitmapMemory, 0, 0);
+    // NOTE DAY 004: VirtualAlloc function
+    // What is a fucking page in memory allocation?
+    // because of we don't know where the memory is, the first parameter is zero
 
+    bitmapMemory = VirtualAlloc(0, bitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 
+    int pitch = width * bytesPerPixel; 
+    uint8_t *row = (uint8_t *) bitmapMemory;
+    for (int y = 0; y < bitmapHeight; ++y)
+    {
+        uint8_t *pixel = (uint8_t *) row;
+        for (int x = 0; x < bitmapWidth; ++x)
+        {
+            /*
+                                       pixel +0 +1 +2 +3 
+            NOTE: pixel structure in memory: 00 00 00 00
+
+            */
+
+           int half = bitmapWidth / 2;
+
+           if (x < half)
+            {
+                *pixel = 0;
+                ++pixel;
+
+                *pixel = 0;
+                ++pixel;
+            
+                *pixel = 255;
+                ++pixel; 
+            
+                *pixel = 100;
+                ++pixel;
+            } 
+            else 
+            {
+                *pixel = 0;
+                ++pixel;
+
+                *pixel = 255;
+                ++pixel;
+            
+                *pixel = 0;
+                ++pixel; 
+            
+                *pixel = 100;
+                ++pixel;
+            }
+        }
+
+        row += pitch;
+
+    }
 }
 
-//hdc -> deviceContext
-
-internal void win32UpdateWindow(HDC hdc, int x, int y, int width, int height)
+internal void win32UpdateWindow(HDC hdc, RECT *windowRect, int x, int y, int width, int height)
 {
-    StretchDIBits(hdc, x, y, width, height, x, y, width, height, bitmapMemory, &bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+    int windowWidth = windowRect->right - windowRect->left;
+    int windowHeight = windowRect->bottom - windowRect->top;
+
+    StretchDIBits(hdc, 0, 0, bitmapWidth, bitmapHeight, 0, 0, windowWidth, windowHeight, bitmapMemory, &bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
 }
-
-
 
 
 LRESULT CALLBACK win32MainWindowCallback(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -74,8 +127,8 @@ LRESULT CALLBACK win32MainWindowCallback(HWND hWnd, UINT msg, WPARAM wParam, LPA
                 int width = clientRect.right - clientRect.left;
                 int height = clientRect.bottom - clientRect.top;
                 win32resizeDIBSelection(width, height);
+                OutputDebugStringA("WM_SIZE\n");
             } break;
-
         case WM_DESTROY:
             {
                 OutputDebugStringA("WM_DESTROY\n");
@@ -97,7 +150,12 @@ LRESULT CALLBACK win32MainWindowCallback(HWND hWnd, UINT msg, WPARAM wParam, LPA
                 int y = paint.rcPaint.top;
                 int width = paint.rcPaint.right - paint.rcPaint.left;
                 int height = paint.rcPaint.bottom - paint.rcPaint.top;
-                win32UpdateWindow(deviceContext, x, y, width, height);
+
+                RECT clientRect;
+                GetClientRect(hWnd, &clientRect);
+
+                // TODO update arguments
+                win32UpdateWindow(deviceContext, &clientRect, x, y, width, height);
                 EndPaint(hWnd, &paint);
             } break; 
         default:
@@ -106,29 +164,27 @@ LRESULT CALLBACK win32MainWindowCallback(HWND hWnd, UINT msg, WPARAM wParam, LPA
               result = DefWindowProc(hWnd, msg, wParam, lParam);
             } break;
     }
-
     return (result);
 }
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-    //TODO
-    WNDCLASS WindowClass = {};
+    WNDCLASS windowClass = {};
 
-    //TODO does style options matter? 
+    // TODO does style options matter? 
     
-    WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
-    WindowClass.lpfnWndProc = win32MainWindowCallback;
-    WindowClass.hInstance = hInstance;
+    windowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+    windowClass.lpfnWndProc = win32MainWindowCallback;
+    windowClass.hInstance = hInstance;
     //WindowClass.hIcon = ;
-    WindowClass.lpszClassName = "ByHandWindowClass";
+    windowClass.lpszClassName = "ByHandWindowClass";
     
-    if (RegisterClass(&WindowClass))
+    if (RegisterClass(&windowClass))
     {
-        HWND WindowHandle = CreateWindowEx(0, WindowClass.lpszClassName, "Byhand Hero",
+        HWND WindowHandle = CreateWindowEx(0, windowClass.lpszClassName, "Byhand Hero",
                 WS_OVERLAPPEDWINDOW | WS_VISIBLE,
                 CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-                CW_USEDEFAULT, 0, 0, WindowClass.hInstance, 0);
+                CW_USEDEFAULT, 0, 0, windowClass.hInstance, 0);
         if (WindowHandle)
         {
             MSG msg;
